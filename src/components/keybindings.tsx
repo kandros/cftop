a150e0e0b3845df72710eb6966e1e4a482ea2863
Jaga Santagostino <jagasantagostino@gmail.com>
2025-11-13 12:50:34 +0100

    when pressing esc go back to home

--- src/components/keybindings.tsx
+++ src/components/keybindings.tsx
@@ -1,7 +1,7 @@
 function Keybindings() {
     return (
         <box borderStyle="single" width="100%" flexDirection="row" flexShrink={0}>
-            <text fg={'#888888'}>q: quit, tab: next section, w: focus workers, d: focus durable objects, b: focus buckets, up: previous item, down: next item, enter: single worker</text>
+            <text fg={'#888888'}>q: quit, esc: back, tab: next section, w: focus workers, d: focus durable objects, b: focus buckets, up: previous item, down: next item, enter: single worker</text>
         </box>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -209,6 +209,12 @@ if (positionals.length == 2) {
                 );
             }
 
+            if (key.name === "escape" || key.name === "esc") {
+                setFocussedSection("workers");
+                setFocussedItem("");
+                setView("home");
+            }
+
             if (key.name === "h") {
                 setFocussedSection("workers");
                 setFocussedItem("");
